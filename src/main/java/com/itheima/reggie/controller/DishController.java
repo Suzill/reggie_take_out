1dc0e0f08941325923ed87688f31805db9ee81fa
suwp <1361075240@qq.com>
2023-02-14 14:23:06 +0800

    springCache配置

--- src/main/java/com/itheima/reggie/controller/DishController.java
+++ src/main/java/com/itheima/reggie/controller/DishController.java
@@ -13,9 +13,13 @@ import com.itheima.reggie.service.DishFlavorService;
 import com.itheima.reggie.service.DishService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /**
@@ -37,14 +41,25 @@ public class DishController {
     @Autowired
     private DishFlavorService dishFlavorService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     /**
      * 新增菜品
      * @param dish 菜品Dto
      * @return return
      */
+    @CacheEvict(value = "setmealCache",allEntries = true)
     @PostMapping
     public R<String> save(@RequestBody DishDto dish) {
         dishService.save(dish);
+        //清理所有菜品缓存数据
+        //Set keys = redisTemplate.keys("dish_*");
+        //redisTemplate.delete(keys);
+
+        //清理某个分类下面的菜品缓存数据
+        String key="dish_"+dish.getCategoryId()+"_"+dish.getStatus();
+        redisTemplate.delete(key);
         return R.success("新增菜成功");
     }
 
@@ -70,16 +85,23 @@ public class DishController {
         return R.success(dishService.getByIdWithFlavor(id));
     }
 
+    @CacheEvict(value = "setmealCache",allEntries = true)
     @PutMapping
     public R<String> updateById(@RequestBody DishDto dishDto) {
         dishService.updateWithFlavor(dishDto);
+        //清理所有菜品缓存数据
+        //Set keys = redisTemplate.keys("dish_*");
+        //redisTemplate.delete(keys);
+
+        //清理某个分类下面的菜品缓存数据
+        String key="dish_"+dishDto.getCategoryId()+"_"+dishDto.getStatus();
+        redisTemplate.delete(key);
         return R.success("修改菜品成功");
     }
 
     //停售起售菜品
     @PostMapping("/status/{status}")
-    public R<String> sale(@PathVariable int status,
-                          String[] ids){
+    public R<String> sale(@PathVariable int status, String[] ids){
         for(String id: ids){
             Dish dish = dishService.getById(id);
             dish.setStatus(status);
@@ -89,6 +111,7 @@ public class DishController {
     }
 
     //删除菜品
+    @CacheEvict(value = "setmealCache",allEntries = true)
     @DeleteMapping
     public R<String> delete(String[] ids){
         for (String id:ids) {
@@ -98,8 +121,19 @@ public class DishController {
     }
 
     //根据条件查询对应菜品数据
+    @Cacheable(value = "setmealCache",key = "#setmeal.categoryId+'_'+#setmeal.status")
     @GetMapping("/list")
     public R<List<DishDto>> list(Dish dish) {
+        // 改造DishController的list方法，先从Redis中获取菜品数据，如果有则直接返回，无需查询数据库
+        List<DishDto> dishDtoList1=null;
+        //动态构造Key
+        String key="dish_"+dish.getCategoryId()+"_"+dish.getStatus();
+        //先从redis中获取缓存数据
+        dishDtoList1 = (List<DishDto>) redisTemplate.opsForValue().get(key);
+        if(dishDtoList1!=null){
+            //如果存在，则直接返回，无需查询数据库
+            return R.success(dishDtoList1);
+        }
 
         //构造查询条件
         LambdaQueryWrapper<Dish> lambdaQueryWrapper = new LambdaQueryWrapper<>();
@@ -110,6 +144,8 @@ public class DishController {
         lambdaQueryWrapper.orderByAsc(Dish::getSort).orderByDesc(Dish::getUpdateTime);
 
         List<Dish> list = dishService.list(lambdaQueryWrapper);
+        //如果不存在，则查询数据库，并且将查询到的菜品数据添加到缓存中
+        redisTemplate.opsForValue().set(key,list,60, TimeUnit.MINUTES);
 
         List<DishDto> dishDtoList = list.stream().map((item) -> {
             DishDto dishDto = new DishDto();
